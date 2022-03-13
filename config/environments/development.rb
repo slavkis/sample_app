7985b267ee2b42c173b4d551f94dc702df4873b2
slavkis <slavko.okunev@gmail.com>
2022-03-13 20:03:11 +0300

    changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -82,8 +82,8 @@ config.action_mailer.default_url_options = { host: host }
 config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :user_name            => 'slavko.okunev@gmail.com',
-  :password             => 'Ridebike2021',
+  :user_name            => '',
+  :password             => '',
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
